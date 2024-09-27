409a79e902e8e9bf381a6a3b8716d2394b5d2ecc
acecution <acecution7@gmail.com>
2024-09-27 21:18:33 +0300

    add pages

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,40 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref, computed } from 'vue'
+import Dashboard from './Dashboard.vue'
+import Projects from './Projects.vue'
+import Tasks from './Tasks.vue'
+import Settings from './Settings.vue'
+
+const routes = {
+  '/': Dashboard,
+  '/projects': Projects,
+  '/tasks': Tasks,
+  '/settings': Settings
+}
+
+const currentPath = ref(window.location.hash)
+
+window.addEventListener('hashchange', () => {
+  currentPath.value = window.location.hash
+})
+
+const currentView = computed(() => {
+  return routes[currentPath.value.slice(1) || '/'] || NotFound
+})
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+  <header></header>
 
-  <main>
-    <TheWelcome />
-  </main>
+  <a href="#/">Dashboard</a> |
+  <a href="#/projects">Projects</a> |
+  <a href="#/tasks">My Tasks</a> |
+  <a href="#/settings">Settings</a> |
+  <component :is="currentView" />
+
+  <main></main>
+
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style scoped></style>
