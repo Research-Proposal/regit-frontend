b960c854ba7950aecf72a62a66d98c6e2ec2dab7
poeticlama <s.knyazkin@innopolis.university>
2024-09-28 17:57:36 +0300

    Corrected main.css

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,3 +1,17 @@
 <template>
-  <h1>Projects</h1>
+  <h1>My Projects</h1>
+  <v-divider></v-divider>
+  <v-list lines="one">
+    <v-list-item
+        v-for="n in 3"
+        :key="n"
+        :title="'Item ' + n"
+        subtitle="Lorem ipsum dolor sit amet consectetur adipisicing elit"
+    ></v-list-item>
+  </v-list>
 </template>
+
+<script>
+
+</script>
+
