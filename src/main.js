66bd3286f1a3bf2294bbe55c6828c136be8c8342
poeticlama <s.knyazkin@innopolis.university>
2024-09-28 14:15:34 +0300

    Import of icons

--- src/main.js
+++ src/main.js
@@ -8,6 +8,13 @@ import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css'
+
+export default createVuetify({
+    icons: {
+        defaultSet: 'mdi',
+    },
+})
 
 const vuetify = createVuetify({
     components,
