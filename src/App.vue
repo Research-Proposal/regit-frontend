eb0ba333db40fc467b311043939b00f16ff7ced7
acecution <acecution7@gmail.com>
2024-09-28 15:23:15 +0300

    fix: routing

--- src/App.vue
+++ src/App.vue
@@ -1,71 +1,73 @@
 <script setup>
-import { ref, computed } from 'vue'
-import Dashboard from './Dashboard.vue'
-import Projects from './Projects.vue'
-import Tasks from './Tasks.vue'
-import Settings from './Settings.vue'
+import { ref, computed } from "vue";
+import Dashboard from "./Dashboard.vue";
+import Projects from "./Projects.vue";
+import Tasks from "./Tasks.vue";
+import Account from "./Account.vue";
 
 const routes = {
-  '/': Dashboard,
-  '/projects': Projects,
-  '/tasks': Tasks,
-  '/settings': Settings
-}
+  "/": Dashboard,
+  "/projects": Projects,
+  "/tasks": Tasks,
+  "/account": Account,
+};
 
-const currentPath = ref(window.location.hash)
+const currentPath = ref(window.location.hash);
 
-window.addEventListener('hashchange', () => {
-  currentPath.value = window.location.hash
-})
+window.addEventListener("hashchange", () => {
+  currentPath.value = window.location.hash;
+});
 
 const currentView = computed(() => {
-  return routes[currentPath.value.slice(1) || '/'] || NotFound
-})
+  return routes[currentPath.value.slice(1) || "/"] || NotFound;
+});
 </script>
 
 <template>
-
   <v-app>
     <v-navigation-drawer>
-      <v-list-item 
-      link
-      title="Dashboard"
-      prepend-icon="mdi-view-dashboard"
-      ></v-list-item>
-      <v-list-item 
-      link
-      title="Projects"
-      prepend-icon="mdi-folder"
-      ></v-list-item>
-      <v-list-item 
-      link
-      title="My Tasks"
-      prepend-icon="mdi-folder"
-      ></v-list-item>
-      <v-list-item 
-      link
-      title="Account"
-      prepend-icon="mdi-account-box"
-      ></v-list-item>
+      <a href="#/">
+        <v-list-item
+          link
+          title="Dashboard"
+          prepend-icon="mdi-view-dashboard"
+        ></v-list-item>
+      </a>
+      <a href="#/projects">
+        <v-list-item
+          link
+          title="Projects"
+          prepend-icon="mdi-folder"
+        ></v-list-item>
+      </a>
+      <a href="#/tasks">
+        <v-list-item
+          link
+          title="My Tasks"
+          prepend-icon="mdi-folder"
+        ></v-list-item>
+      </a>
+      <a href="#/account">
+        <v-list-item
+          link
+          title="Account"
+          prepend-icon="mdi-account-box"
+        ></v-list-item>
+      </a>
     </v-navigation-drawer>
 
     <v-app-bar title="App bar"></v-app-bar>
 
     <v-main>
       <v-container>
-        <h1>Main Content</h1>
+        <component :is="currentView" />
       </v-container>
     </v-main>
-
   </v-app>
-
-  <!-- <a href="#/">Dashboard</a> |
-  <a href="#/projects">Projects</a> |
-  <a href="#/tasks">My Tasks</a> |
-  <a href="#/settings">Settings</a> | -->
-
-  <component :is="currentView" />
-  
 </template>
 
-<style scoped></style>
+<style scoped>
+a {
+  color: darkorange;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,24 +1,24 @@
-import './assets/main.css'
+import "./assets/main.css";
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
 // Vuetify
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
-import '@mdi/font/css/materialdesignicons.css'
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+import "@mdi/font/css/materialdesignicons.css";
 
 export default createVuetify({
-    icons: {
-        defaultSet: 'mdi',
-    },
-})
+  icons: {
+    defaultSet: "mdi",
+  },
+});
 
 const vuetify = createVuetify({
-    components,
-    directives,
-  })
-  
-  createApp(App).use(vuetify).mount('#app')
+  components,
+  directives,
+});
+
+createApp(App).use(vuetify).mount("#app");
